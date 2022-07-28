445991cea43e5e30f6b16c36ad5b2059b423af5b
Synchroz <juliyanto1071@student.untan.ac.id>
2022-07-28 08:15:57 +0700

    ...

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # begin properties
 script="$0"
 properties() { '
-kernel.string=Luuvy kernel from Zamrud Khatulistiwa
+kernel.string=Auguri 12 kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
